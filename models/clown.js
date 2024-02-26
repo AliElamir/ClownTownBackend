1cdfc53f5efb00b3e5518c00d1365880b9850a4b
najeeb2442 <s.najib2442@gmail.com>
2024-02-26 20:36:28 +0300

    done with models

--- models/clown.js
+++ models/clown.js
@@ -1,16 +1,14 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
-const roomSchema = new Schema(
+const clownSchema = new Schema(
   {
     name: { type: String, required: true },
-    desc: { type: String, required: true },
     image: { type: String, required: true },
-    clowns: [{ type: Schema.Types.ObjectId, ref: "Clown" }],
   },
   {
     timestamps: true,
   }
 )
 
-module.exports = mongoose.model("Room", roomSchema)
+module.exports = mongoose.model("Clown", clownSchema)
