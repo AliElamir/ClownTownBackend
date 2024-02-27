edc30a60dad2d9d42bd152f17ae1fda991143d6c
najeeb2442 <s.najib2442@gmail.com>
2024-02-27 08:24:08 +0300

    created clowns model and controllers

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var logger = require("morgan")
 
 var indexRouter = require("./routes/index")
 var roomsRouter = require("./routes/rooms")
+var clownsRouter = require("./routes/clowns")
 
 var app = express()
 
@@ -22,8 +23,9 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, "public")))
 
-app.use("/", indexRouter)
+// app.use("/", indexRouter)
 app.use("/rooms", roomsRouter)
+app.use("/clowns", clownsRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
